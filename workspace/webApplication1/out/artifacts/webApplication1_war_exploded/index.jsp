09ea3317b61c5639c8082c6e1a37b10f32ca2478
Jay Jaganathan <jay.jaganathan1@gmail.com>
2015-03-16 20:41:21 -0400

    Eclipse

---
+++ workspace/webApplication1/out/artifacts/webApplication1_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jmrs004
+  Date: 3/14/2015
+  Time: 7:23 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+ <h1>Hello World!</H1> </body>
+</html>
---
+++ workspace/webApplication1/src/com/jj/Servlet.java
@@ -0,0 +1,16 @@
+package com.jj;
+
+import java.io.IOException;
+
+/**
+ * Created by jmrs004 on 3/14/2015.
+ */
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+response.getWriter().write("Hello World!");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
